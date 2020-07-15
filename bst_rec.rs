5385784fd1d526674c4effbd2aa5149f048fdda0
Oliver Calder <oacalder@gmail.com>
2020-07-15 12:32:18 -0500

    Fixed bugs, compiles and works. Important things: Box allocates memory to the heap, returns a smart pointer; match statements should probably use & reference for whatever is being matched; matching with Option<> is lovely.

--- bst_rec.rs
+++ bst_rec.rs
@@ -1,30 +1,29 @@
 struct Node {
     val:    u32,
-    left:   Option<Node>, // In order to allow the possibility of null values,
-    right:  Option<Node>  // wrap such variables in the Option enum.
-}
-
-fn new_node(val: u32) -> Node {
-    Node {val, None, None}
+    left:   Option<Box<Node>>, // In order to allow the possibility of null values,
+    right:  Option<Box<Node>>  // wrap such variables in the Option enum.
 }
 
 impl Node {
+    fn new(newval: u32) -> Box<Node> {
+        Box::new(Node {val: newval, left: None, right: None})
+    }
     // Returns true if value unique, false if value was already in tree
     fn add(&mut self, newval: u32) -> bool {
         if newval == self.val {
             return false;
         } else if newval < self.val {
-            return match self.left {
+            return match &mut self.left {
                 None => {
-                    self.left = Some(new_node(newval));
+                    self.left = Some(Node::new(newval));
                     true
                 },
                 Some(n) => n.add(newval)
             }
         } else {
-            return match self.right {
+            return match &mut self.right {
                 None => {
-                    self.right = Some(new_node(newval));
+                    self.right = Some(Node::new(newval));
                     true
                 },
                 Some(n) => n.add(newval)
@@ -35,24 +34,24 @@ impl Node {
     fn contains(&self, val: u32) -> bool {
         if val == self.val { return true }
         else if val < self.val {
-            return match self.left {
+            return match &self.left {
                 None => false,
                 Some(n) => n.contains(val)
             }
         } else {
-            return match self.right {
+            return match &self.right {
                 None => false,
                 Some(n) => n.contains(val)
             }
         }
     }
     fn print(&self) -> () {
-        match self.left {
+        match &self.left {
             None => {},
             Some(n) => n.print()
         }
         println!("{}", self.val);
-        match self.right {
+        match &self.right {
             None => {},
             Some(n) => n.print()
         }
@@ -60,9 +59,11 @@ impl Node {
 }
 
 fn main() {
-    let mut tree = new_node(6);
+    let mut tree = Node::new(6);
     for val in vec![2, 7, 5, 3, 1, 9, 4, 9] {
         tree.add(val);
     }
-    tree.print()
+    tree.print();
+    println!("Contains 3? {}", tree.contains(3));
+    println!("Contains 8? {}", tree.contains(8));
 }
